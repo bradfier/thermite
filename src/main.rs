02401e96f0020a98266ca2a39fcc24d234556186
Richard Bradfield <richard.bradfield@hp.com>
2015-11-03 11:54:02 +0000

    Update gitignore, add LICENSE to dir and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * Thermite - An I/O generation tool in Rust
+ * Copyright (C) 2015 Richard Bradfield
+ *                                                                         
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *                                                                         
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *                                                                         
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 extern crate getopts;
 extern crate rand;
 extern crate num;
